fa19c7057e3a0f70992b771082654c392f776106
liuqitao <liuqitao@corp.netease.com>
2026-02-09 16:01:04 +0800

    pad

--- lightvllm/llm.py
+++ lightvllm/llm.py
@@ -31,7 +31,6 @@ class LLM:
         
         if attention_mask is None:
             attention_mask = [[1] * len(p) for p in prompts]
-        attention_mask = torch.tensor(attention_mask, dtype=torch.long, device="cuda")
         
         done = [False] * len(prompts)
 
@@ -43,11 +42,12 @@ class LLM:
             active_indices = [i for i, f in enumerate(done) if not f]
 
             active_input_ids = [prompts[i] for i in active_indices]
-            active_attention_mask = [attention_mask[i] for i in active_indices]
-            
             input_ids = torch.tensor(active_input_ids, dtype=torch.long, device="cuda")
 
             positions = torch.arange(0, input_ids.shape[1], dtype=torch.long, device="cuda")
+
+            active_attention_mask = [attention_mask[i] for i in active_indices]
+            active_attention_mask = torch.tensor(active_attention_mask, dtype=torch.long, device="cuda")
 
             logits = self.model(positions, input_ids, attention_mask=active_attention_mask)
             next_tokens = self.sampler(logits, temperatures[active_indices])
